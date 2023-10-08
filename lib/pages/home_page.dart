1d36867a6cbf5b5727f61b4beed71c96ca7157ec
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2023-10-09 00:05:05 +0300

    small change in look

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,44 +6,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: RichText(
-          text: const TextSpan(
-            children: [
-              TextSpan(
-                text: "M",
-                style: TextStyle(
-                  color: Colors.white, // Color for the "M" letter
-                  fontWeight: FontWeight.bold, // Make it bold
-                  fontSize: 30.0, // Increase font size
-                  fontFamily: "ConcertOne", // Font family
-                ),
-              ),
-              TextSpan(
-                text: "AI",
-                style: TextStyle(
-                  color: Colors.red, // Color for the "AI" letters
-                  fontWeight: FontWeight.bold, // Make it bold
-                  fontSize: 30.0, // Color for the "AI" letters
-                  fontFamily: "ConcertOne", // Font family
-                ),
-              ),
-              TextSpan(
-                text: "NFULL",
-                style: TextStyle(
-                  color: Colors.white, // Color for the "NFULL" letters
-                  fontWeight: FontWeight.bold, // Make it bold
-                  fontSize: 30.0, // Color for the "NFULL" letters
-                  fontFamily: "ConcertOne", // Font family
-                ),
-              ),
-            ],
-          ),
-        ),
         backgroundColor: Colors.black,
-      ),
-      body: const Placeholder(),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: RichText(
+            text: const TextSpan(
+              children: [
+                TextSpan(
+                  text: "M",
+                  style: TextStyle(
+                    color: Colors.white, // Color for the "M" letter
+                    fontWeight: FontWeight.bold, // Make it bold
+                    fontSize: 30.0, // Increase font size
+                    fontFamily: "ConcertOne", // Font family
+                  ),
+                ),
+                TextSpan(
+                  text: "AI",
+                  style: TextStyle(
+                    color: Colors.red, // Color for the "AI" letters
+                    fontWeight: FontWeight.bold, // Make it bold
+                    fontSize: 30.0, // Color for the "AI" letters
+                    fontFamily: "ConcertOne", // Font family
+                  ),
+                ),
+                TextSpan(
+                  text: "NDFUL",
+                  style: TextStyle(
+                    color: Colors.white, // Color for the "NFULL" letters
+                    fontWeight: FontWeight.bold, // Make it bold
+                    fontSize: 30.0, // Color for the "NFULL" letters
+                    fontFamily: "ConcertOne", // Font family
+                  ),
+                ),
+              ],
+            ),
+          ),
+          backgroundColor: Colors.transparent,
+        ),
+        body: const Placeholder());
   }
 }
