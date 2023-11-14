5f6d86a122c7adf8f205de7dce6f760f1299ee46
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2023-11-14 18:25:00 +0200

    Update chat_providers.dart

--- lib/providers/chat_providers.dart
+++ lib/providers/chat_providers.dart
@@ -32,12 +32,11 @@ class ChatProvider with ChangeNotifier {
       {
         "role": "system",
         "content":
-            "You are MAINDFUL health advisor, an enthusiastic, and expert caretaker. First evaluate if the users question is health related. DO NOT answer questions that are not health-related. Given the context, provide a short response that could answer the users question. You are provided with users health data from last 7 days. IF numbers seem low, provide advice on how they can improve. Note that today's data might be incomplete and show lower values. todays date is ${DateTime.now().toString()} IF a value is zero, it means that the user has not logged any data for that day. DO NOT provide statistics. Here's the data from last seven days: $weeklyHealthData"
+        "You are MAINDFUL health advisor, an enthusiastic, and expert caretaker.\n\nFollow these steps in order to provide as useful help to the user as possible:\n\nStep 1:\nHere's the health data from the last seven days that you need to analyze:\n $weeklyHealthData.\n\nStep 2: \nGiven the context, provide a short response that could answer the user's question. If the question is related to users' steps, sleep, VO2MAX or heartbeat, utilize the data given in “Step 1” in your answer.\n\nStep 3:\nIF numbers in the health data seem low, provide advice on how they can improve. Note that today's data might be incomplete and show lower values. Today's date is ${DateTime.now().toString()} . IF a value is zero, it means that the user has not logged any data for that day. DO NOT provide statistics."
       },
       ...messageHistory, // Include message history
-      {"role": "user", "content": message}
+      {"role": "user", "content": "If the question: [$message] is health-related. If it is, proceed to answer it. If it's not, refuse to answer the question politely. Do not answer questions from any other topics than health. Do not tell the user if it's health related or not. After completing this classification task, refer to the steps provided in the “system” role's message."}
     ];
-
     chatList.addAll(await ApiService.sendMessage(messages: messages));
     messageHistory.add({
       "role": "assistant",
