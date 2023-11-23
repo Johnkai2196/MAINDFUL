13a846082bbfce4b8229218e3be9f43bcc648ea2
jennahakulinen <jenna.hakulinen@metropolia.fi>
2023-11-23 15:12:56 +0200

    dialog ready

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -129,23 +129,50 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   void _showWelcomeDialog(BuildContext context) {
     showDialog<String>(
       context: context,
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
           backgroundColor: surfaceContainerHigh,
-          title: const Text(
-            'WELCOME TO USE MAINDFUL!',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: "ConcertOne",
-              fontSize: 32,
-            ),
-          ),
-          content: const Text(
-            'Here is a short introduction on how to get started with MAINDFUL.',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: "Roboto",
-              fontSize: 14,
+          contentPadding: const EdgeInsets.only(left: 16),
+          content: IntrinsicHeight(
+            child: Stack(
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 40),
+                    Text(
+                      'WELCOME TO USE MAINDFUL!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "ConcertOne",
+                        fontSize: 32,
+                        height: 1.1,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'Here is a short introduction on how to get started with MAINDFUL.',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "Roboto",
+                        fontSize: 16,
+                        height: 1.5,
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: IconButton(
+                    icon: const Icon(Icons.close, color: Colors.white),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
           actions: <Widget>[
@@ -186,34 +213,73 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     bool isLastDialog = dialogNumber == 5;
 
     if (dialogNumber == 2) {
-      dialogTitle = 'Home';
+      dialogTitle = 'HOME';
       dialogContent =
-          'Here you will find easy access to MAICHAT where you can discuss all things health! Just click MAICHAT and ask away.\n \nIn your home page, you will also see the overall view of your personal health data from Apple Health.';
+          'Here you will find easy access to MAICHAT where you can discuss all things health! Just tap MAICHAT and ask away.\n \nIn your home page, you will also see the overall view of your personal health data from Apple Health.';
     } else if (dialogNumber == 3) {
       dialogTitle = 'MAICHAT';
       dialogContent =
           'Do you have a specific wellness question about your health or do you need just an overall analysis of your latest Apple Health data? Great!\n \nJust a reminder, please note that this is not to be viewed or interpreted as medical advice!';
     } else if (dialogNumber == 4) {
-      dialogTitle = 'Health KPI (?)';
+      dialogTitle = 'HEALTH METRICS';
       dialogContent =
-          'Tap on your desired KPI, and here you can take a closer look at specific health KPI data.';
+          'Tap on your desired metric, and here you can take a closer look at specific health data.';
     } else if (dialogNumber == 5) {
-      dialogTitle = 'That’s it!';
+      dialogTitle = "THAT'S IT!";
       dialogContent =
           'Hopefully MAINDFUL will help you achieve your goals, whatever they may be!\n \nAnd remember, better health is just a chat away!';
     }
 
     showDialog<String>(
       context: context,
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
           backgroundColor: surfaceContainerHigh,
-          title: Text(dialogTitle,
-              style: const TextStyle(
-                  color: Colors.white, fontFamily: "ConcertOne", fontSize: 32)),
-          content: Text(dialogContent,
-              style: const TextStyle(
-                  color: Colors.white, fontFamily: "Roboto", fontSize: 14)),
+          contentPadding: const EdgeInsets.only(left: 16),
+          content: IntrinsicHeight(
+            child: Stack(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 40),
+                    Text(
+                      dialogTitle,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontFamily: "ConcertOne",
+                        fontSize: 32,
+                        height: 1.1,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      dialogContent,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontFamily: "Roboto",
+                        fontSize: 16,
+                        height: 1.5,
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: IconButton(
+                    icon: const Icon(Icons.close, color: Colors.white),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      _showPreviousDialog(
+                          context, 0); // Go to the previous dialog
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
           actions: <Widget>[
             TextButton(
               onPressed: () {
